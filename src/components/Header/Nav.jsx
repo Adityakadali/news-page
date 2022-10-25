e8c0494e5df2d4ed349baf2c0a601c046621b320
aditya <adityakadali00@gmail.com>
2022-10-26 01:12:25 +0530

    refactored navbar

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -4,50 +4,50 @@ import { useState } from "react";
 
 function Nav() {
   const navItems = ["Home", "New", "Popular", "Trending", "Categories"];
-  const [navState, setNavState] = useState(true);
+  const [navState, setNavState] = useState(false);
 
-  if (navState == true) {
-    return (
-      <nav role={"navigation"}>
-        <ul className="hidden gap-4 md:flex">
-          {navItems.map((e, i) => (
-            <li key={i} className="text-slate-700 hover:text-orange-400">
-              <a href={`/${e}`}>{e}</a>
-            </li>
-          ))}
-        </ul>
-        <button
-          className="absolute right-6 top-6"
-          onClick={() => setNavState(!navState)}
-        >
-          <img className="md:hidden" src={navState ? open : close} alt="" />
-        </button>
-      </nav>
-    );
-  } else {
-    return (
-      <nav
-        role={"navigation"}
-        className="absolute top-0 right-0 grid h-screen w-56 place-content-center bg-white py-4"
+  return (
+    <nav role={"navigation"}>
+      <ul className="hidden gap-4 md:flex">
+        {navItems.map((e, i) => (
+          <li key={i} className="text-slate-700 hover:text-orange-400">
+            <a className="text-lg font-semibold" href={`/${e}`}>
+              {e}
+            </a>
+          </li>
+        ))}
+      </ul>
+      <button
+        className="absolute right-6 top-6"
+        onClick={() => setNavState(!navState)}
       >
-        <button
-          className="absolute right-6 top-6"
-          onClick={() => setNavState(!navState)}
-        >
-          <img className="md:hidden" src={navState ? open : close} alt="" />
-        </button>
-        <ul className="flex flex-col gap-5">
-          {navItems.map((e, i) => (
-            <li key={i} className="text-slate-700 hover:text-orange-400">
-              <a className="text-xl" href={`/${e}`}>
-                {e}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    );
-  }
+        <img className="md:hidden" src={navState ? close : open} alt="" />
+      </button>
+      {navState ? (
+        <>
+          <div className="absolute top-0 right-0 h-screen w-9/12 bg-white py-40 px-6 md:hidden">
+            <button
+              className="absolute right-6 top-6"
+              onClick={() => setNavState(!navState)}
+            >
+              <img className="md:hidden" src={navState ? close : open} alt="" />
+            </button>
+            <ul className="flex flex-col gap-5 ">
+              {navItems.map((e, i) => (
+                <li key={i} className="text-slate-700 hover:text-orange-400">
+                  <a className="text-xl font-semibold" href={`/${e}`}>
+                    {e}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="absolute inset-0 -z-10 bg-black opacity-20"></div>
+        </>
+      ) : (
+        <></>
+      )}
+    </nav>
+  );
 }
-
 export default Nav;
